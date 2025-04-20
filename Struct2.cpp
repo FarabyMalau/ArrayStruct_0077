e8ab6ddf74e2c0e7ab606a2b5aa6d227b579f31f
FarabyMalau <farabymuzakki49@gmail.com>
2025-04-20 15:08:01 +0600

    Membuat input untuk data mahasiswa dan display data mahasiswa

--- Struct2.cpp
+++ Struct2.cpp
@@ -5,3 +5,35 @@ using namespace std;
 struct detailAlamat{
     string desa;
     string kota;
+};
+
+//membuat struct mahasiswa
+struct mahasiswa{
+    string nim;
+    string nama;
+    detailAlamat alamat;
+};
+
+int main(){
+    //membuat objek mhs untuk struct mahasiswa pada program utama
+    mahasiswa mhs;
+
+    //Membuat input untuk Data Mahasiswa
+    cout << "Masukkan NIM = ";
+    cin >> mhs.nim;
+    cout << "Masukkan Nama = ";
+    cin >> mhs.nama;
+    cout << "Masukkan Alamat Desa = ";
+    cin >> mhs.alamat.desa;
+    cout << "Masukkan Alamat Kota = ";
+    cin >> mhs.alamat.kota;
+
+    cout << endl;
+     //Display Data Mahasiswa
+     cout << "Data Mahasiswa" << endl;
+    
+     cout << "NIM = " << mhs.nim << endl;
+     cout << "Nama = " << mhs.nama << endl;
+     cout << "Alamat Desa = " << mhs.alamat.desa << endl;
+     cout << "Alamat Kota = " << mhs.alamat.kota << endl;
+}
