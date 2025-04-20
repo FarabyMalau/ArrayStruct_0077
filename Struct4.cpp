1faeb68da72976a3f6e23c6675d8630d2181def0
FarabyMalau <farabymuzakki49@gmail.com>
2025-04-20 15:17:19 +0600

    Perulangan untuk mengulangi sebanyak index array input

--- Struct4.cpp
+++ Struct4.cpp
@@ -17,3 +17,20 @@ struct mahasiswa{
 int main(){
     //menentukan banyak data yang akan dimasukkan ke array
     mahasiswa mhs[2];
+
+    for(int i = 0; i < 2; i++){ //perulangan untuk mengulangi sebanyak index array
+        cout << "Mahasiswa ke-" << i+1 << endl;
+    cout << "Masukkan NIM = ";
+    cin >> mhs[i].nim;
+    cin.ignore();
+    cout << "Masukkan Nama = ";
+    getline(cin, mhs[i].nama);
+    cout << "Masukkan Alamat Desa = ";
+    cin >> mhs[i].alamat.desa;
+    cout << "Masukkan Alamat Kota = ";
+    cin >> mhs[i].alamat.kota;
+    }
+
+    cout << endl;
+    //Display Data Mahasiswa
+    cout << "Data Mahasiswa" << endl;
