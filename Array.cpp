e0564090a21631db3d05dd75956f0868bd710a65
FarabyMalau <farabymuzakki49@gmail.com>
2025-04-20 14:58:04 +0600

    Menampilkan array dengan loop for

--- Array.cpp
+++ Array.cpp
@@ -19,3 +19,9 @@ int main(){
         cout << "Nama Mahasiswa ke-" << i+1 << " = ";
         cin >> nama[i];
     }
+    cout<<endl;
+    //menampilkan array dengan loop for
+    for(int i = 0; i < 3; i++){
+        cout << "Data Mahasiswa ke-" << i+1 << " = " << nama[i]
+        << endl;
+    }
